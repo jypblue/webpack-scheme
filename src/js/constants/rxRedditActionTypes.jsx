bd3da262f5dd3cd3a14d4e8ada51d3f5781dc646
jypblue <1421160774@qq.com>
2016-07-17 12:04:25 +0800

    add react-redux async demo

---
+++ src/js/constants/rxRedditActionTypes.jsx
@@ -0,0 +1,11 @@
+/**
+ *
+ * @authors zx.wang (zx.wang1991@gmail.com)
+ * @date    2016-07-17 11:55:23
+ * @version $Id$
+ */
+
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT';
+export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT';
